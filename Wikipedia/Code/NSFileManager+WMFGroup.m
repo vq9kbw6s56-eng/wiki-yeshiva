4b89ab438d222e09081cbc131b4711b2cfd2b3d8
vq9kbw6s56-eng <289846131+vq9kbw6s56-eng@users.noreply.github.com>
2026-06-02 10:05:59 +0000

    Fallback when app group container is unavailable

--- Wikipedia/Code/NSFileManager+WMFGroup.m
+++ Wikipedia/Code/NSFileManager+WMFGroup.m
@@ -6,7 +6,18 @@ NSString *const WMFApplicationGroupIdentifier = @QUOTE(WMF_APP_GROUP_IDENTIFIER)
 @implementation NSFileManager (WMFGroup)
 
 - (nonnull NSURL *)wmf_containerURL {
-    return [self containerURLForSecurityApplicationGroupIdentifier:WMFApplicationGroupIdentifier];
+    NSURL *containerURL = [self containerURLForSecurityApplicationGroupIdentifier:WMFApplicationGroupIdentifier];
+    if (containerURL != nil) {
+        return containerURL;
+    }
+
+    NSURL *applicationSupportURL = [self URLsForDirectory:NSApplicationSupportDirectory inDomains:NSUserDomainMask].firstObject;
+    NSURL *fallbackURL = [applicationSupportURL URLByAppendingPathComponent:@"Wikipedia" isDirectory:YES];
+
+    NSError *error = nil;
+    [self createDirectoryAtURL:fallbackURL withIntermediateDirectories:YES attributes:nil error:&error];
+
+    return fallbackURL;
 }
 
 - (nonnull NSString *)wmf_containerPath {
